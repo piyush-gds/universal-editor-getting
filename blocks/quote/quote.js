ae7fcdf6be4b7f160919a08407daf8b51da32331
Piyush Jain <piyush.jain17@gds.ey.com>
2025-08-21 18:09:57 +0530

    css quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,9 +3,11 @@ export default function decorate(block) {
   const authorText = block.children[1].textContent.trim();
 
   const quotePara = document.createElement("p");
+  quotePara.classList.add('quote-para');
   quotePara.textContent = quoteText;
 
   const authorPara = document.createElement("p");
+  authorPara.classList.add('author-para');
   authorPara.textContent = authorText;
 
   block.replaceChildren(quotePara, authorPara);
