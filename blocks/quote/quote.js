eff3f242ce11d123dd08096e38edd761cdfb1fed
Piyush Jain <piyush.jain17@gds.ey.com>
2025-08-21 17:06:40 +0530

    quote

---
+++ blocks/quote/quote.js
@@ -0,0 +1,10 @@
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  console.log("block.children: ", block.children);
+  console.log("quoteWrapper: ", quoteWrapper);
+
+  const blockquote = document.createElement("blockquote");
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
